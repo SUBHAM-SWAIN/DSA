6306f8007b241d22f77a43f182e59709b01f477b
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-07-15 22:54:50 +0530

    Initial commit

---
+++ Array/array1.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class array1 {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Print the elements of the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+
+        // Calculate the sum of the elements in the array
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
