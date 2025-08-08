f8be15bc61e9c9ba62743737d2f111816866d06d
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-08 14:51:52 +0530

    Added

--- Array/Array3.java
+++ Array/Array3.java
@@ -2,16 +2,18 @@ package Array;
 
 public class Array3 {
     public static void main(String[] args) {
-        int arr[] = { 4, 5, 6, 3, 7 };
-        int num = 7;
+        int arr[] = { 4, 5, 6, 3, 7, 8, 4, 5 };
+        int num = 6;
         int sum = 0;
         Boolean isSorted = true;
         int count = 0;
         int divisibleBy = 0;
         int largest = Integer.MIN_VALUE;
-        Boolean isUnique = true;
         int unique = 0;
+        int SecondLargest = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
+            Boolean isUnique = true;
+
             // find index
             if (num == arr[i]) {
                 System.out.println("at index:" + i);
@@ -22,6 +24,7 @@ public class Array3 {
             }
             // largest
             if (arr[i] > largest) {
+                SecondLargest = largest;
                 largest = arr[i];
             }
 
@@ -39,11 +42,16 @@ public class Array3 {
             }
 
             // uniqe element
-            for (int j = i; j < arr.length; j++) {
-                if (arr[i] != arr[j]) {
-                    unique++;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    isUnique = false;
                 }
             }
+
+            if (isUnique) {
+                unique++;
+            }
+
         }
         System.out.println("Largest:" + largest);
         if (isSorted) {
@@ -51,7 +59,8 @@ public class Array3 {
         } else {
             System.out.println("Not sorted");
         }
-        System.out.println("Divisible By 3 :" + divisibleBy);
+        System.out.println("Divisible By 3:" + divisibleBy);
+        System.out.println("Second Largest:" + SecondLargest);
         System.out.println("sum " + sum);
         System.out.println("even " + count);
         System.out.println("odd:" + (arr.length - count));
