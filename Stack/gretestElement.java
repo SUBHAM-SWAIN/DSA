9309f580fbe7b00b9e5fa6b8e63e1c2279c553bc
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-11 11:52:47 +0530

    Added

---
+++ Stack/gretestElement.java
@@ -0,0 +1,29 @@
+package Stack;
+
+import java.util.*;
+
+public class gretestElement {
+    public static void main(String[] args) {
+        int[] arr = { 2, 6, 3, 1, 8, 2, 5, 7, 4 };
+        Stack<Integer> s = new Stack<>();
+        int nextGreater[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && s.peek() <= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = s.peek();
+            }
+            s.push(arr[i]);
+
+        }
+
+        for (int i = 0; i < nextGreater.length; i++) {
+            System.out.println("Next greater element for " + arr[i] + " is: " + nextGreater[i]);
+        }
+
+    }
+}
