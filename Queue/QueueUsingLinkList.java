136823c41d97ef974c259924759102065ce287d1
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-16 22:24:44 +0530

    Queue 2st problem

---
+++ Queue/QueueUsingLinkList.java
@@ -0,0 +1,66 @@
+package Queue;
+
+public class QueueUsingLinkList {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class Queue {
+        Node head = null;
+        Node tail = null;
+
+        public boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int frontData = head.data;
+            head = head.next;
+            if (head == null) {
+                tail = null; // If queue becomes empty
+            }
+            return frontData;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
