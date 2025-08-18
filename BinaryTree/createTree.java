f4f459fbaa0e68eb034d59db0164a78352af90bc
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-18 21:07:51 +0530

    aa

--- BinaryTree/createTree.java
+++ BinaryTree/createTree.java
@@ -38,16 +38,27 @@ public class createTree {
             preorder(root.right);
         }
 
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
     }
 
     public static void main(String[] args) {
-        int nodes[] = { 1, 4, 5, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
         BinaryTree b1 = new BinaryTree();
         Node root = b1.createTree(nodes);
         System.out.println(root.data);
 
         b1.preorder(root);
+        System.out.println(" ");
+        b1.inorder(root);
 
     }
 }
