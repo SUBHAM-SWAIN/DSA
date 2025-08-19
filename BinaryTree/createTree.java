360f25921412f1e0c4f5d0f5fe80afeb06b45545
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-19 09:49:27 +0530

    height

--- BinaryTree/createTree.java
+++ BinaryTree/createTree.java
@@ -94,6 +94,17 @@ public class createTree {
 
     }
 
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
@@ -108,6 +119,15 @@ public class createTree {
         b1.postorder(root);
         System.out.println("");
         b1.levelOrderTraverse(root);
+
+        Node root2 = new Node(1);
+        root2.left = new Node(2);
+        root2.right = new Node(3);
+        root2.left.left = new Node(4);
+        root2.left.right = new Node(5);
+        root2.right.left = new Node(6);
+        root2.right.right = new Node(7);
+        System.out.println(height(root2));
 
     }
 }
