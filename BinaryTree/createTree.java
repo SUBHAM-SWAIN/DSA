b380729014e18b9b9d9bad570c103beac58abac2
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-20 23:46:01 +0530

    kndk

--- BinaryTree/createTree.java
+++ BinaryTree/createTree.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -190,6 +191,54 @@ public class createTree {
         return isIdentical(root.left, subroot) || isIdentical(root.right, subroot);
     }
 
+    static class Info2 {
+        Node node;
+        int hd;
+
+        Info2(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+
+    }
+
+    public static void topView(Node root) {
+        if (root == null)
+            return;
+
+        Queue<Info2> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+
+        q.add(new Info2(root, 0));
+
+        while (!q.isEmpty()) {
+            Info2 curr = q.remove();
+
+            // store first node of each horizontal distance
+            if (!map.containsKey(curr.hd)) {
+                map.put(curr.hd, curr.node);
+            }
+
+            if (curr.node.left != null) {
+                q.add(new Info2(curr.node.left, curr.hd - 1));
+                min = Math.min(min, curr.hd - 1);
+            }
+
+            if (curr.node.right != null) {
+                q.add(new Info2(curr.node.right, curr.hd + 1));
+                max = Math.max(max, curr.hd + 1);
+            }
+        }
+
+        System.out.println("TOP VIEW IS:");
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
@@ -212,6 +261,7 @@ public class createTree {
         root2.left.right = new Node(5);
         root2.right.left = new Node(6);
         root2.right.right = new Node(7);
+        topView(root2);
         System.out.println(height(root2));
         System.out.println(countNodes(root2));
 
