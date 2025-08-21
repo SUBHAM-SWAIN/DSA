b0c2847fa79efa118b40fc2893fcda509080782e
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-21 09:49:40 +0530

    Added

--- Heap/Priority_Queue.java
+++ Heap/Priority_Queue.java
@@ -4,15 +4,33 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Priority_Queue {
+
+    static class Student implements Comparable<Student> {
+        String Name;
+        int rank;
+
+        Student(String Name, int rank) {
+            this.Name = Name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            return this.rank - o.rank; // Ascending order based on rank
+        }
+
+    }
+
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(4);
-        pq.add(0);
-        pq.add(1);
-        pq.add(7);
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(new Student("A", 1));
+        pq.add(new Student("B", 12));
+        pq.add(new Student("C", 4));
+        pq.add(new Student("D", 45));
 
         while (!pq.isEmpty()) {
-            System.out.println(pq.remove());
+            System.out.println(pq.peek().Name + " " + pq.peek().rank);
+            pq.remove();
         }
     }
 }
