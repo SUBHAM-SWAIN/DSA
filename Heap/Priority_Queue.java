cfdb8758c5502b0ef5456ff8eb26c439faafac55
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-22 15:28:23 +0530

    added

--- Heap/Priority_Queue.java
+++ Heap/Priority_Queue.java
@@ -34,3 +34,65 @@ public class Priority_Queue {
         }
     }
 }
+
+// ..............................
+class Solution {
+    public static int minCost(int[] arr) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
+        }
+
+        int cost = 0;
+        while (pq.size() > 1) {
+            int min1 = pq.remove();
+            int min2 = pq.remove();
+            cost = cost + min1 + min2;
+            pq.add(min1 + min2);
+        }
+        return cost;
+    }
+}
+
+// ...................
+class Solution {
+
+    static class Row implements Comparable<Row> {
+        int soldiers;
+        int idx;
+
+        Row(int soldiers, int idx) {
+            this.soldiers = soldiers;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Row r2) {
+            if (this.soldiers == r2.soldiers) {
+                return this.idx - r2.idx;
+            } else {
+                return this.soldiers - r2.soldiers;
+            }
+        }
+
+    }
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+        for (int i = 0; i < mat.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mat[0].length; j++) {
+                count += mat[i][j];
+            }
+            pq.add(new Row(count, i));
+        }
+
+        int res[] = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.remove().idx;
+        }
+        return res;
+    }
+}
